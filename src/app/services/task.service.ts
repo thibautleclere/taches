f416ccd80dea3dd772787b56741c0c68439d270c
thibaut <thibaut.leclere@gmail.com>
2022-12-29 15:48:48 +0100

    save and load tasks

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ITasks } from '../interfaces';
+import * as fileSaver from 'file-saver';
 
 @Injectable()
 export class TaskService {
@@ -18,6 +19,19 @@ export class TaskService {
 
     public getProjects(): string[] {
         return [...new Set(this.getTasks().map((t) => t.project || '').filter((p) => !!p))];
+    }
+
+    public saveTasksOnFile(): void {
+        const sTasks: string = JSON.stringify(this.getTasks());
+        const blob = new Blob([sTasks], {type: 'application/json'});
+        fileSaver.saveAs(blob, this.getToday());
+    }
+
+    private getToday(): string {
+        const today: Date = new Date();
+        return today.getDay().toString().padStart(2, '0')
+          +today.getMonth().toString().padStart(2, '0')
+          +today.getFullYear();
     }
 
 }
